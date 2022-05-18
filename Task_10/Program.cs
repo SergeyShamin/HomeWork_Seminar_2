549f5fb89cfb5ba8f7d4dc35ef879be0c9b63977
SergeyShamin <shamins61@yandex.ru>
2022-05-18 22:27:09 +0300

    finish task 10

---
+++ Task_10/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход
+ трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+Console.WriteLine("Введите число: ");
+string input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+int secondDight = number / 10 % 10;
+Console.WriteLine (secondDight);
